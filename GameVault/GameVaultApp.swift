d38caa8ee190053b5629c1853d0e87fe4e8fd51e
eintmyatthu <eaintmyatthu1997@gmail.com>
2026-09-13 20:54:00 +0700

    Initial Commit

---
+++ GameVault/GameVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameVaultApp.swift
+//  GameVault
+//
+//  Created by Eaint Myat Thu on 13/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct GameVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
